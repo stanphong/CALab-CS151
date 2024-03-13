bc9ea64cbdf8028b9e9bd68851677108345b2166
Minh Phong Do <mipodo1052@gmail.com>
2024-03-13 12:19:12 -0700

    mvc Model and View

---
+++ src/mvc/View.java
@@ -0,0 +1,16 @@
+package mvc;
+
+import javax.swing.*;
+
+public class View extends JPanel implements Subscriber{
+    protected Model model;
+
+    public View(Model model){
+        this.model = model;
+        model.subscribe(this);
+    }
+    @Override
+    public void update() {
+
+    }
+}
