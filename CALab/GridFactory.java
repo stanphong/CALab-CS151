b1bf86c37db933dc2721a8b27fd14cfe63b4dd5a
alexpoulos.ken <alexopoulos.ken@gmail.com>
2024-03-17 19:28:56 -0700

    Yep

---
+++ CALab/GridFactory.java
@@ -0,0 +1,56 @@
+package CALab;
+
+import mvc.AppFactory;
+import mvc.Command;
+import mvc.Model;
+
+import mvc.*;
+
+public class GridFactory implements AppFactory {
+
+    @Override
+    public Model makeModel() {
+        return new Grid();
+    }
+
+    @Override
+    public View makeView(Model model) {
+        // Ensure that the model is indeed an instance of Grid before casting.
+        return new GridView((Grid)model);
+    }
+
+
+    @Override
+    public String getTitle() {
+        return null;
+    }
+
+    @Override
+    public String getHelp() {
+        return null;
+    }
+
+    @Override
+    public String about() {
+        return null;
+    }
+
+    @Override
+    public String[] getEditCommands() {
+        return new String[]{"Run1" , "Run50" , "Populate" , "Clear"};
+    }
+
+    @Override
+    public Command makeEditComand(Model m, String type, Object source) {
+        if (type == "Run1"){
+            return new RunCommand(m, 1);
+        } else if (type == "Run50") {
+            return new RunCommand(m, 50);
+        } else if (type == "Populate") {
+
+            return new PopulateCommand();
+        } else {
+            return new ClearCommand();
+        }
+    }
+}
